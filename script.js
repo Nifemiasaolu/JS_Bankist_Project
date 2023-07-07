d595e47ebd1b975379a9ed4ffc026efdcbeec071
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-07-07 17:29:35 +0100

    Script Functionality

--- script.js
+++ script.js
@@ -417,7 +417,7 @@ const deposit5 = mov => mov > 0;
 // console.log(movements.filter(deposit5));
 // console.log(movements.every(deposit5));
 
-//============= Flat Method ===========
+//============= Flat Method =============
 const arr = [[1,2,3], [4,5,6], [7,8,9]]
 console.log(arr.flat());
 
